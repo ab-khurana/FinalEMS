2f75f3d893b31ca65688632f11b5a042f4ac3b4f
ab-khurana <abhi.abhishekkhurana@yahoo.in>
2016-09-28 11:21:20 +0530

    Alert message added

--- EMSProject/src/com/cfs/ems/controller/UpdateProjectAllocationServlet.java
+++ EMSProject/src/com/cfs/ems/controller/UpdateProjectAllocationServlet.java
@@ -3,6 +3,7 @@ package com.cfs.ems.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,31 +12,39 @@ import javax.servlet.http.HttpServletResponse;
 import com.cfs.ems.service.LoginInterface;
 import com.cfs.ems.service.LoginService;
 
+public class UpdateProjectAllocationServlet extends HttpServlet {
 
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 
-public class UpdateProjectAllocationServlet extends HttpServlet{
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-boolean st=false;
-		
-		PrintWriter out = response.getWriter();  
-	LoginInterface upe=new LoginService();
-	
-	try {
-		st=upe.editProjectAllocation1(request);
-	} catch (Exception e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	System.out.println("Dan");
-	if(st)
-	{
-	out.write(" Updated Successfully ");	
-	}
-	else{
-		out.write("failed to update");
-	}
+		boolean status = false;
+
+		PrintWriter out = response.getWriter();
+		LoginInterface upe = new LoginService();
+
+		try {
+			status = upe.editProjectAllocation1(request);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		if (status) {
+			/*RequestDispatcher rd = request
+					.getRequestDispatcher("/updateprojectAllocation");
+			rd.include(request, response);
+			out.println("<html><body onload=\"alert('Data Updated Successfully')\"></body></html>");*/
+			out.println("yess");
+			
+		} else {
+
+			/*RequestDispatcher rd = request
+					.getRequestDispatcher("listOfProjects.jsp");
+			rd.include(request, response);
+			out.println("<html><body onload=\"alert('Data Not Updated')\"></body></html>");*/
+			
+			out.println("noo");
+		}
 	}
 
 }
