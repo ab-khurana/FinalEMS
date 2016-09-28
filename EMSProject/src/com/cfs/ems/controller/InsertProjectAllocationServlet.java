886992869adbd51df453c0d0b645ffada7c08559
ab-khurana <abhi.abhishekkhurana@yahoo.in>
2016-09-28 10:46:17 +0530

    alert message added

--- EMSProject/src/com/cfs/ems/controller/InsertProjectAllocationServlet.java
+++ EMSProject/src/com/cfs/ems/controller/InsertProjectAllocationServlet.java
@@ -12,42 +12,39 @@ import javax.servlet.http.HttpServletResponse;
 import com.cfs.ems.service.LoginInterface;
 import com.cfs.ems.service.LoginService;
 
+public class InsertProjectAllocationServlet extends HttpServlet {
 
-
-
-public class InsertProjectAllocationServlet extends HttpServlet{
-
-	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-	     
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+
 		PrintWriter out = response.getWriter();
-				
-			
-				
-			
-					LoginInterface inf = new LoginService();
-					boolean status=false;
-			
-						try {
-							status = inf.projectAllocations(request);
-						} catch (Exception e) {
-							
-							e.printStackTrace();
-						}
-					
-					if(status) {
-						out.print("Employee Allocated to project Successfully!");
-//						RequestDispatcher rd = request.getRequestDispatcher("projectAllocation.jsp");
-//						rd.forward(request, response); 
-						
-						
-					} else {
-						out.print("Failed to Allocated employee...!!!");
-					}
-			}
+
+		LoginInterface inf = new LoginService();
+		boolean status = false;
+
+		try {
+			status = inf.projectAllocations(request);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		if (status) {
+			RequestDispatcher rd = request
+					.getRequestDispatcher("projectAllocation.jsp");
+			rd.include(request, response);
+			out.println("<html><body onload=\"alert('Project Allocated Successfully!')\"></body></html>");
+
+		} else {
+			RequestDispatcher rd = request
+					.getRequestDispatcher("projectAllocation.jsp");
+			rd.include(request, response);
+			out.println("<html><body onload=\"alert('UnSuccessfull Allocation')\"></body></html>");
+		}
+	}
 }
